91dd32f9f5eb1f5e83ef7b0f7faceefbea7ec390
Techboy v1050 <tokkivanc13@gmail.com>
2021-06-01 15:04:26 +0300

    Le book class.

--- src/com/techboy/java/classes/Book.java
+++ src/com/techboy/java/classes/Book.java
@@ -8,7 +8,7 @@ public class Book {
 	public String letter_size;
 	public Book(String title, String author, int pages, String language, String letter_size) {
 		this.title = title;
-		this.title = title;
+		this.author = author;
 		this.title = title;
 		this.title = title;
 		this.title = title;
