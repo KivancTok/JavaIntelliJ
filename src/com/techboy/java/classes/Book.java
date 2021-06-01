43f4b95e3f4491211b14cfd438011b613a52ae19
Techboy v1050 <tokkivanc13@gmail.com>
2021-06-01 15:01:28 +0300

    The book class of my project.

---
+++ src/com/techboy/java/classes/Book.java
@@ -0,0 +1,16 @@
+package com.techboy.java.classes;
+
+public class Book {
+	public String title;
+	public String author;
+	public int pages;
+	public String language;
+	public String letter_size;
+	public Book(String title, String author, int pages, String language, String letter_size) {
+		this.title = title;
+		this.title = title;
+		this.title = title;
+		this.title = title;
+		this.title = title;
+	}
+}
